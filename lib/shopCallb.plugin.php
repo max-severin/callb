9c71802b8f5d09d1b57682dd04f905f176e4cf25
Max Severin <makc.severin@gmail.com>
2015-05-03 01:53:13 +0300

    added settingNumberControl method in shopCallbPlugin

--- lib/shopCallb.plugin.php
+++ lib/shopCallb.plugin.php
@@ -20,10 +20,63 @@ class shopCallbPlugin extends shopPlugin {
 
     }
 
-    // public function settingCustomControl() {           
+    /**
+     * 
+     * @param string $name
+     * @param array $params
+     * @return string
+     */
+    static public function settingNumberControl($name, $params = array()) {
 
-    //     //       
+        $control = '';
 
-    // }
+        $control_name = htmlentities($name, ENT_QUOTES, 'utf-8');
+        
+        $control .= "<input id=\"{$params['id']}\" type=\"number\" name=\"{$control_name}\" ";
+        $control .= self::addCustomParams(array('class', 'placeholder', 'value',), $params);
+        $control .= self::addCustomParams(array('min', 'max', 'step',), $params['options']);
+        $control .= ">";
+
+        return $control;
+
+    }
+
+    /**
+     *
+     * @param array $list
+     * @param array $params
+     * @return string
+     */
+    private static function addCustomParams($list, $params = array()) {
+
+        $params_string = '';
+
+        foreach ($list as $param => $target) {
+            if (is_int($param)) {
+                $param = $target;
+            }
+            if (isset($params[$param])) {
+                $param_value = $params[$param];
+                if (is_array($param_value)) {
+                    if (isset($param_value['title'])) {
+                        $param_value = $param_value['title'];
+                    } else {
+                        $param_value = implode(' ', $param_value);
+                    }
+                }
+                if ($param_value !== false) {
+                    $param_value = htmlentities((string)$param_value, ENT_QUOTES, 'utf-8');
+                    if (in_array($param, array('autofocus'))) {                    	
+                        $params_string .= " {$target}";
+                    } else {                    	
+                        $params_string .= " {$target}=\"{$param_value}\"";
+                    }
+                }
+            }
+        }
+
+        return $params_string;
+
+    }
 
 }
