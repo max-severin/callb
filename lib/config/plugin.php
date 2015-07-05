2d892cdccb7e638c1e68754bdcd318ff83d952e1
Max Severin <makc.severin@gmail.com>
2015-07-05 22:24:48 +0300

    v1.1.0 init

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -5,7 +5,7 @@
  */
 return array(
     'name' => /*_wp*/('Callback'),
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'img' => 'img/callb.png',
     'vendor' => 1020720,
     'shop_settings' => true,
