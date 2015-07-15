ac28e73408f114125a0838bab60544c2cb869f04
Max Severin <makc.severin@gmail.com>
2015-07-15 14:40:41 +0300

    info fix

--- js/callb.backend.settings.js
+++ js/callb.backend.settings.js
@@ -68,7 +68,7 @@ var callbBackendSettings = (function () { "use strict";
         tipBlock.addClass('tip-block').prepend(
             '<h3 id="tip-show"><span>{_wp("Tip for setting up")}</span></h3>' + 
             '<p>{_wp("1) To use the plugin in the Shop app switch on hook frontend_head and scripts are automatically loaded in your template.")}</p>' + 
-            '<p>{_wp("2) To use the plugin in other apps it is necessary to insert in the template of your application to the end of the tag &#060;head&#062; the following code:")}</p>' + 
+            '<p>{_wp("2) To use the plugin in other apps it is necessary to switch off hook frontend_head and to insert in the template of your application to the end of the tag &#060;head&#062; the following code:")}</p>' + 
             '<p><b>&#123;if $wa->shop&#125;&#123;shopCallbPlugin::display()&#125;&#123;/if&#125;</b></p>' + 
             '<p>{_wp("3) To bind the form to the html-element in the template, you must either create a new or use an existing one.")}</p>' + 
             '<p>{_wp("For example, you have the template has the following element:")}<br />' + 
